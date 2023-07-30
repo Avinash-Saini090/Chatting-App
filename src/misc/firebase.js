92e182d060fa8f0ac8b88e54c9d11da4954a7f36
Half mind <avisaini909@gmail.com>
2023-07-30 17:22:41 +0530

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+
+const config = {
+    apiKey: "AIzaSyCpptRsi7e_RMz2hmTlNmGw5ltCc6lQCmY",
+    authDomain: "chat-web-app-3ee14.firebaseapp.com",
+    projectId: "chat-web-app-3ee14",
+    storageBucket: "chat-web-app-3ee14.appspot.com",
+    messagingSenderId: "237941037845",
+    appId: "1:237941037845:web:b28b3c242037b66168e71c"
+};
+
+const app = firebase.initializeApp(config);
